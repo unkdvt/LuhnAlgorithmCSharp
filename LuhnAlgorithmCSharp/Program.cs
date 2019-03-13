893445f64159d2d443c692bee633a603d01869a2
lakmal <lakmal@DESKTOP-ONMRTNV>
2019-03-13 22:58:12 +0530

    Update LuhnAlgorithmCSharp/Program.cs

--- LuhnAlgorithmCSharp/Program.cs
+++ LuhnAlgorithmCSharp/Program.cs
@@ -10,6 +10,47 @@ namespace LuhnAlgorithmCSharp
     {
         static void Main(string[] args)
         {
+            r: Console.Write("Enter Card Number or type Q or q to exit : ");
+            string input = Console.ReadLine();
+            if (input.ToUpper().Equals("Q"))
+                Environment.Exit(0);
+            bool x = check(input);
+            if (x)
+                Console.WriteLine("Valid Card");
+            else
+                Console.WriteLine("Invalid Card");
+
+            goto r;
+
+        }
+
+        /// <summary>
+        /// Luhn Algorithm C#
+        /// </summary>
+        /// <param name="ccNumber"></param>
+        /// <returns></returns>
+        public static bool check(string ccNumber)
+        {
+            int sum = 0;
+            bool alternate = false;
+            for (int i = ccNumber.Length - 1; i >= 0; i--)
+            {
+                char[] nx = ccNumber.ToArray();
+                int n = int.Parse(nx[i].ToString());
+
+                if (alternate)
+                {
+                    n *= 2;
+
+                    if (n > 9)
+                    {
+                        n = (n % 10) + 1;
+                    }
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return (sum % 10 == 0);
         }
     }
 }
