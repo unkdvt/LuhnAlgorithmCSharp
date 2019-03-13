28a180576ff7c73c2f4d2022c7b4d6aeb604c6e2
lakmal <lakmal@DESKTOP-ONMRTNV>
2019-03-13 23:11:47 +0530

    update detail comments

---
+++ LuhnAlgorithmCSharp/Luhn.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuhnAlgorithmCSharp
+{
+    public static class Luhn
+    {
+        /// <summary>
+        /// Luhn Algorithm C#
+        /// This class is created on 13 March 2019, at 17:17 (UTC).
+        /// by Lakmal http://github.com/l4km47
+        /// </summary>
+        /// <param name="ccNumber"></param>
+        /// <returns></returns>
+        public static bool check(string ccNumber)
+        {
+            int sum = 0;
+            bool alternate = false;
+            for (int i = ccNumber.Length - 1; i >= 0; i--)
+            {
+                char[] nx = ccNumber.ToArray();
+                int n = int.Parse(nx[i].ToString());
+
+                if (alternate)
+                {
+                    n *= 2;
+
+                    if (n > 9)
+                    {
+                        n = (n % 10) + 1;
+                    }
+                }
+                sum += n;
+                alternate = !alternate;
+            }
+            return (sum % 10 == 0);
+        }
+    }
+}
--- LuhnAlgorithmCSharp/Program.cs
+++ LuhnAlgorithmCSharp/Program.cs
@@ -14,7 +14,7 @@ namespace LuhnAlgorithmCSharp
             string input = Console.ReadLine();
             if (input.ToUpper().Equals("Q"))
                 Environment.Exit(0);
-            bool x = check(input);
+            bool x = Luhn.check(input);
             if (x)
                 Console.WriteLine("Valid Card");
             else
@@ -24,33 +24,6 @@ namespace LuhnAlgorithmCSharp
 
         }
 
-        /// <summary>
-        /// Luhn Algorithm C#
-        /// </summary>
-        /// <param name="ccNumber"></param>
-        /// <returns></returns>
-        public static bool check(string ccNumber)
-        {
-            int sum = 0;
-            bool alternate = false;
-            for (int i = ccNumber.Length - 1; i >= 0; i--)
-            {
-                char[] nx = ccNumber.ToArray();
-                int n = int.Parse(nx[i].ToString());
 
-                if (alternate)
-                {
-                    n *= 2;
-
-                    if (n > 9)
-                    {
-                        n = (n % 10) + 1;
-                    }
-                }
-                sum += n;
-                alternate = !alternate;
-            }
-            return (sum % 10 == 0);
-        }
     }
 }
